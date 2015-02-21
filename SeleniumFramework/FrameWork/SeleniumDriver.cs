56facb1d60c89347f793c86f9a927e1179f5fa76
cheryjose@gmail.com <cheryjose@gmail.com>
2015-02-21 12:25:43 +0800

    Framework classes

---
+++ SeleniumFramework/FrameWork/SeleniumDriver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using OpenQA.Selenium.Chrome;
+using FrameWork = SeleniumFramework.FrameWork;
+
+namespace SeleniumFramework.FrameWork
+{
+    public class SeleniumDriver
+    {
+        private ChromeOptions chromeOptions;
+        public ChromeDriver ChromeDriver;
+        private enum ChromeOptionsKey
+        {
+            ChromeDriverPath
+        }
+
+        public enum Driver
+        {
+            Chrome,
+            Ie,
+            Firefox
+        }
+
+        public SeleniumDriver(Driver driver)
+        {
+            switch(driver)
+            {
+                case Driver.Chrome:
+                    chromeOptions = new ChromeOptions();
+                    ChromeDriver = new ChromeDriver(SetChromeOptions());
+                    break;
+                case Driver.Ie:
+                    break;
+                case Driver.Firefox:
+                    break;
+            }
+        }
+
+        public ChromeOptions SetChromeOptions()
+        {
+            var binaryLocation = ConfigurationManager.AppSettings[ChromeOptionsKey.ChromeDriverPath.ToString()];
+            chromeOptions.BinaryLocation = binaryLocation;
+            return chromeOptions;
+        }
+    }
+}
--- SeleniumFramework/FrameWork/UIOperations/EditBox.cs
+++ SeleniumFramework/FrameWork/UIOperations/EditBox.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SeleniumFramework
+namespace SeleniumFramework.FrameWork.UIOperations
 {
-    public class Class1
+    class EditBox
     {
     }
 }
